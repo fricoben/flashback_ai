239d165622db5081076f8b049c1d7934448be99a
Fricoben <benjamin.flores.pro@gmail.com>
2025-12-01 17:34:10 +0000

    fix: legacy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
-import { baseURL } from "./baseUrl";
 
-const nextConfig: NextConfig = {
-  assetPrefix: baseURL,
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
